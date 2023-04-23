fc02984dc90f5ac892f34d99f5d7a583d14aee33
Melvin Sembrano <melv@hey.com>
2023-04-23 23:21:40 +1000

    precompiled assets

--- lib/tailwind/scaffold/engine.rb
+++ lib/tailwind/scaffold/engine.rb
@@ -5,6 +5,12 @@ module Tailwind
     class Engine < ::Rails::Engine
       isolate_namespace Tailwind::Scaffold
 
+      initializer 'tailwind-scaffold.assets.precompile' do |app|
+        app.config.assets.precompile += %w[
+          tailwind-scaffold/application.css
+        ]
+
+      end
     end
   end
 end
